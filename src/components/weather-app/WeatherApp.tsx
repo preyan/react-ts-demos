86cb311a3d7e85d2eb8c50126e2782357eaf8d7b
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2023-12-23 17:45:04 +0530

    Updated design and optimised the component

--- src/components/weather-app/WeatherApp.tsx
+++ src/components/weather-app/WeatherApp.tsx
@@ -1,144 +1,149 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from "react";
+import { WeatherData, GeoData } from "./WeatherAppModels";
 
-interface GeoData {
-  name: string;
-  local_names?: { [key: string]: string };
-  lat: number;
-  lon: number;
-  country: string;
-  state: string;
+interface WeatherDetailsProps {
+  weatherData?: WeatherData;
 }
 
-export interface WeatherData {
-  coord: Coord;
-  weather: Weather[];
-  base: string;
-  main: Main;
-  visibility: number;
-  wind: Wind;
-  rain: Rain;
-  clouds: Clouds;
-  dt: number;
-  sys: Sys;
-  timezone: number;
-  id: number;
-  name: string;
-  cod: number;
-}
+const API_BASE_URL = "http://api.openweathermap.org";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
-export interface Clouds {
-  all: number;
-}
+const getApiUrl = (path: string, params: Record<string, string>) => {
+  const queryString = new URLSearchParams(params).toString();
+  return `${API_BASE_URL}${path}?${queryString}&appid=${API_KEY}`;
+};
 
-export interface Coord {
-  lon: number;
-  lat: number;
-}
+const fetchData = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching data from ${url}:`, error);
+    throw error;
+  }
+};
 
-export interface Main {
-  temp: number;
-  feels_like: number;
-  temp_min: number;
-  temp_max: number;
-  pressure: number;
-  humidity: number;
-  sea_level: number;
-  grnd_level: number;
-}
+const LoadingSpinner: FC = () => (
+  <div className="flex items-center justify-center">
+    <div className="animate-spin rounded-full border-t-4 border-blue-500 border-opacity-50 border-solid h-16 w-16"></div>
+  </div>
+);
 
-export interface Rain {
-  '1h': number;
-}
+const WeatherDetails: FC<WeatherDetailsProps> = ({ weatherData }) => {
+  const renderWeatherIcon = (icon: string) => {
+    const iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+    return <img src={iconUrl} alt="Weather Icon" />;
+  };
 
-export interface Sys {
-  type: number;
-  id: number;
-  country: string;
-  sunrise: number;
-  sunset: number;
-}
+  const renderWeatherDetails = () => {
+    if (!weatherData) {
+      return <p>No weather data available.</p>;
+    }
 
-export interface Weather {
-  id: number;
-  main: string;
-  description: string;
-  icon: string;
-}
+    const { name, main, weather, wind, clouds } = weatherData;
 
-export interface Wind {
-  speed: number;
-  deg: number;
-  gust: number;
-}
+    return (
+      <>
+        <h2 className="text-xl font-bold mb-2">Current Weather in {name}</h2>
+        <div className="flex items-center">
+          <div className="mr-4">{renderWeatherIcon(weather[0].icon)}</div>
+          <div>
+            <p>Temperature: {main?.temp}°C</p>
+            <p>Feels Like: {main?.feels_like}°C</p>
+            <p>Description: {weather[0]?.description}</p>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p>Humidity: {main?.humidity}%</p>
+          <p>Wind Speed: {wind?.speed} m/s</p>
+          <p>Cloudiness: {clouds?.all}%</p>
+        </div>
+      </>
+    );
+  };
+
+  return <div className="weather-details">{renderWeatherDetails()}</div>;
+};
 
 const WeatherApp: FC = () => {
-  const API_KEY = import.meta.env.VITE_API_KEY;
   const [weatherData, setWeatherData] = useState<WeatherData>();
   const [geoData, setGeoData] = useState<GeoData>();
-
   const [loading, setLoading] = useState<boolean>(false);
-  const [city, setCity] = useState<string>('');
+  const [city, setCity] = useState<string>("");
 
   const fetchGeoData = async () => {
     try {
       setLoading(true);
-      const geoDataResponse = await (
-        await fetch(
-          `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`
-        )
-      ).json();
+      const geoDataResponse = await fetchData(
+        getApiUrl("/geo/1.0/direct", { q: city })
+      );
       setGeoData(geoDataResponse[0]);
     } catch (error) {
       setLoading(false);
-      console.error('Error fetching geo data:', error);
     }
   };
+
   useEffect(() => {
-    if (geoData) fetchWeatherData();
+    if (geoData) {
+      const { lat, lon } = geoData;
+      fetchWeatherData(lat, lon);
+    }
   }, [geoData]);
-  const fetchWeatherData = async () => {
+
+  const fetchWeatherData = async (lat: number, lon: number) => {
     try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${geoData?.lat}&lon=${geoData?.lon}&appid=${API_KEY}`
+      const weatherDataResponse = await fetchData(
+        getApiUrl("/data/2.5/weather", {
+          lat: lat.toString(),
+          lon: lon.toString(),
+        })
       );
-      const weatherDataResponse = await response.json();
       setWeatherData(weatherDataResponse);
       setLoading(false);
     } catch (error) {
       setLoading(false);
-      console.error('Error fetching weather data:', error);
     }
   };
 
-  return (
-    <div className='container mx-auto p-4'>
-      <h1 className='text-3xl font-bold mb-4'>Weather App</h1>
-      <div className='bg-gray-200 p-4 rounded-lg'>
-        {loading ? (
-          <div className='flex items-center justify-center'>
-            <div className='animate-spin rounded-full border-t-4 border-blue-500 border-opacity-50 border-solid h-16 w-16'></div>
-          </div>
-        ) : (
-          <>
-            <h2 className='text-xl font-bold mb-2'>Current Weather</h2>
-            <p>City: {weatherData?.name}</p>
-            <p>Temperature: {weatherData?.main?.temp}°C</p>
-            <p>Description: {weatherData?.weather[0].description}</p>
-          </>
-        )}
-      </div>
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    fetchGeoData();
+  };
 
-      <div>
-        <label>City</label>
+  return (
+    <div className="container mx-auto p-4 bg-blue-100 rounded-md shadow-md">
+      <h1 className="text-3xl font-bold mb-4 text-center text-blue-800">
+        Weather App
+      </h1>
+
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-center mb-4">
+        <label htmlFor="city" className="mr-2 text-blue-800">
+          City
+        </label>
         <input
-          type='text'
-          name='city'
-          className='bg-red-300'
+          type="text"
+          id="city"
+          name="city"
+          className="border p-2 rounded-md"
           onChange={(e) => setCity(e.target.value)}
         />
-        <button type='submit' onClick={() => fetchGeoData()}>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white ml-2 p-2 rounded-md">
           Submit
         </button>
+      </form>
+
+      <div className="bg-gray-200 p-4 rounded-lg">
+        {loading ? (
+          <div className="flex items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        ) : (
+          <WeatherDetails weatherData={weatherData} />
+        )}
       </div>
     </div>
   );
