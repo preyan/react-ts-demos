dcededb9e55264c7d807a3c4db2eb5ad4de41e1b
Preyan Bhowmick <30199639+preyan@users.noreply.github.com>
2023-12-23 17:44:39 +0530

    Added models

---
+++ src/components/weather-app/WeatherAppModels.ts
@@ -0,0 +1,75 @@
+interface GeoData {
+  name: string;
+  local_names?: { [key: string]: string };
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
+
+/**
+ * Represents the weather data for a location.
+ */
+interface WeatherData {
+  coord: Coord;
+  weather: Weather[];
+  base: string;
+  main: Main;
+  visibility: number;
+  wind: Wind;
+  rain: Rain;
+  clouds: Clouds;
+  dt: number;
+  sys: Sys;
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+interface Clouds {
+  all: number;
+}
+
+interface Coord {
+  lon: number;
+  lat: number;
+}
+
+interface Main {
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  pressure: number;
+  humidity: number;
+  sea_level: number;
+  grnd_level: number;
+}
+
+interface Rain {
+  "1h": number;
+}
+
+interface Sys {
+  type: number;
+  id: number;
+  country: string;
+  sunrise: number;
+  sunset: number;
+}
+
+interface Weather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface Wind {
+  speed: number;
+  deg: number;
+  gust: number;
+}
+
+export type { GeoData, WeatherData };
